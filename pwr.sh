38a032b5332e1fc644414ae2b664f396eeb7002b
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-12-16 14:20:28 +0700

    Update pwr.sh

--- pwr.sh
+++ pwr.sh
@@ -261,7 +261,7 @@ status=\$(echo \$exStr | jq -r .status);
 msgTg=\$(echo -e \"ℹ️ * Your PWR Validator Info * ℹ️ \n\n Voting Power: \${votePwr} \n Address: \\\`\${addrPwr}\\\` \n Last Created Block Time : \${lastBTq} \n IP Address: \\\`\${ipVal}\\\` \n Delegators Count: \${delCount} \n Last Created Block: \${lastCB} \n Total Shares: \${totalShr} \n Status: \${status} \n\nCreator: [Mr9868 ☕](https://www\\.github\\.com/mr9868)\")
 
 curl -s -X POST https://api.telegram.org/bot\${tgApiQn}/sendMessage -d chat_id=\${tgIdQn} -d text=\"\${msgTg}\" -d parse_mode='MarkdownV2' 2>/dev/null;
-
+echo;
 echo 'Telegram message sent !';
 
 { cekLastCB=\$(curl \$urlCek\$pwrAddr | jq -r .validator.lastCreatedBlock); } 2>/dev/null;
