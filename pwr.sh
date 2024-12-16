d2604df3d290b3bca5ad38434d496d6d50dd68c4
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-12-16 11:39:22 +0700

    Update pwr.sh

--- pwr.sh
+++ pwr.sh
@@ -324,7 +324,7 @@ then
 mkdir ~/.mr9868
 mkdir ~/.mr9868/pwr
 fi
-echo "" > ~/.mr9868/pwr/config;
+
 if grep -wq "tgApiQn" ~/.mr9868/pwr/config; then    
 sudo pkill -f "ewmLog"
 sed -r -i "s/tgApiQn=.*/tgApiQn=${tgApiQn}/g" ~/.mr9868/pwr/config
