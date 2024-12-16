165fecbd487d7f8952ff0bdd8669c67373d5a415
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-12-16 14:55:44 +0700

    Update pwr.sh

--- pwr.sh
+++ pwr.sh
@@ -259,7 +259,7 @@ lastCB=\$(echo \$exStr | jq -r .lastCreatedBlock);
 totalShr=\$(echo \$exStr | jq -r .totalShares);
 status=\$(echo \$exStr | jq -r .status);
 
-msgTg=\$(echo -e \"ℹ️ * Your PWR Validator Info * ℹ️ \n\n Voting Power: \${votePwr} \n Address: \\\`\${addrPwr}\\\` \n Last Created Block Time : \${lastBTq}  \n Last Created Block : \${lastBTO} \n IP Address: \\\`\${ipVal}\\\` \n Delegators Count: \${delCount} \n Last Created Block Number: \${lastCB} \n Total Shares: \${totalShr} \n Status: \${status} \n\nCreator: [Mr9868 ☕](https://www\\.github\\.com/mr9868)\")
+msgTg=\$(echo -e \"ℹ️ * Your PWR Validator Info * ℹ️ \n\n 🔸Voting Power: \${votePwr} \n 🔸Address: \\\`\${addrPwr}\\\` \n 🔸Last Created Block Time : \${lastBTq}  \n 🔸IP Address: \\\`\${ipVal}\\\` \n 🔸Delegators Count: \${delCount} \n 🔸Last Created Block: \${lastCB} \n 🔸Status: \${status} \n\nCreator: [Mr9868 ☕](https://www\\.github\\.com/mr9868)\")
 
 curl -s -X POST https://api.telegram.org/bot\${tgApiQn}/sendMessage -d chat_id=\${tgIdQn} -d text=\"\${msgTg}\" -d parse_mode='MarkdownV2' 2>/dev/null;
 echo;
