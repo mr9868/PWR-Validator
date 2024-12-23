0f998e816ec52184dee3232eb6cd7c216288194d
Muhammad Nur <116989757+mr9868@users.noreply.github.com>
2024-12-23 11:16:20 +0700

    Update pwr.sh

--- pwr.sh
+++ pwr.sh
@@ -33,12 +33,12 @@ command -v jq >/dev/null 2>&1 || { echo >&2 "JQ is not found on this machine, In
 # My Header
 function myHeader(){
 clear;
-echo -e "============================================================"
-echo -e "=             PWR validator setup auto installer           ="
-echo -e "=                    Created by : Mr9868                   ="
-echo -e "=             Github : https://github.io/Mr9868            ="
-echo -e "=                Your OS info : $(uname -s) $(uname -m)              ="
-echo -e "=                 IP Address : ${myIP}               ="
+echo  "============================================================"
+echo  "=             PWR validator setup auto installer           ="
+echo  "=                    Created by : Mr9868                   ="
+echo  "=             Github : https://github.io/Mr9868            ="
+echo  "=                Your OS info : $(uname -s) $(uname -m)              ="
+echo  "=                 IP Address : ${myIP}               ="
 echo -e "============================================================\n"
 }
 
