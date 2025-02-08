a29d802cfcab974f08552a60c21f4d58198f986a
Muhammad Nur <116989757+mr9868@users.noreply.github.com>
2025-02-08 08:00:50 +0700

    Update pwr.sh

--- pwr.sh
+++ pwr.sh
@@ -397,7 +397,7 @@ bash ~/.mr9868/pwr/tgInit;
 
 echo '
 pwrAddr=$( curl localhost:8085/address/ 2>/dev/null );
-sed -r -i "s/pwrAddr=.*/pwrAddr=\${pwrAddr}/g"  ~/.mr9868/pwr/config;
+sed -r -i "s/pwrAddr=.*/pwrAddr=${pwrAddr}/g"  ~/.mr9868/pwr/config;
 screen -X -S pwrBot quit;
 screen -dmS pwrBot bash -c "chmod +x ~/.mr9868/pwr/tgServer && bash ~/.mr9868/pwr/tgServer";
 echo "Starting screen ..."
