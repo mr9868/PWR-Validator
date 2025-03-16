0ea82e8479455c670b40ef5ea031e6a42ff61f1f
Muhammad Nur <116989757+mr9868@users.noreply.github.com>
2025-03-16 09:09:01 +0700

    Update pwr.sh

--- pwr.sh
+++ pwr.sh
@@ -391,14 +391,14 @@ msgTg=\$( echo -e \" \
  🔸Address: \\\`0x\${addrPwr}\\\` \n \
  🔸Last block number: \${lastCB} \n \
  🔸Status: \${status} \n \
+ 🔸Validator info: [Go to the Explorer](https://explorer\\.pwrlabs\\.io/address/0x\${addrPwr}) \n \ 
  🔸New block info: \n \
     🔹Block number: \${blockNumber} \n \
     🔹Block transaction count: \${blockTxCount} Tx\n \
     🔹Timestamp: \${diffBT} \n \
     🔹Block reward: \${blockReward} PWR \n \
     🔹Block Details: [Go to the Explorer](https://explorer\\.pwrlabs\\.io/blocks/\${lastCB}) \n \
- 🔸Total blocks created: \${totalBlocks} \n \
- 🔸Validator info: [Go to the Explorer](https://explorer\\.pwrlabs\\.io/address/0x\${addrPwr}) \n\n \
+ 🔸Total blocks created: \${totalBlocks} \n\n \
 Creator: [Mr9868 ☕](https://www\\.github\\.com/mr9868)\");
 
 echo -e '[INFO] Sending telegram message ... ⏳';
